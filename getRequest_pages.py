b8ee7bc529a8960182349f6adc3437a34352e6d4
Anass Boukhems <anass.boukhems@qucit.com>
2023-06-19 23:15:58 +0200

    save to csv

--- getRequest_pages.py
+++ getRequest_pages.py
@@ -1,12 +1,12 @@
+import csv
 import requests
 from bs4 import BeautifulSoup
-import pymongo
 
 def scrape_quotes():
     more_links = True
     page = 1
     quotes = []
-    while(more_links):
+    while more_links:
         markup = requests.get(f'http://quotes.toscrape.com/page/{page}').text
         soup = BeautifulSoup(markup, 'html.parser')
         for item in soup.select('.quote'):
@@ -15,13 +15,14 @@ def scrape_quotes():
             quote['author'] = item.select_one('.author').get_text()
             tags = item.select_one('.tags')
             quote['tags'] = [tag.get_text() for tag in tags.select('.tag')]
+            quote['page'] = page  # Add the page number to the quote
             quotes.append(quote)
 
         next_link = soup.select_one('.next > a')
 
-        print(f'scraped page {page}')
+        print(f'Scraped page {page}')
 
-        if(next_link):
+        if next_link:
             page += 1
         else:
             more_links = False
@@ -29,4 +30,12 @@ def scrape_quotes():
 
 quotes = scrape_quotes()
 
-print(quotes)
+# Save quotes to a CSV file
+filename = 'quotes.csv'
+with open(filename, 'w', newline='', encoding='utf-8') as file:
+    fieldnames = ['text', 'author', 'tags', 'page']  # Include 'page' in fieldnames
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerows(quotes)
+
+print(f"Quotes saved to {filename}.")
